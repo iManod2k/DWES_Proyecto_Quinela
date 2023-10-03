af4b2f5148fb7cfdda9c64e46ea8cb6a79aca510
Charles M <carlosmaquedajimenez@gmail.com>
2023-10-03 10:35:40 +0200

    Add files via upload

--- Quiniela.php
+++ Quiniela.php
@@ -2,7 +2,11 @@
 <HEAD>
     <TITLE> EJ1-Conversion IP Decimal a Binario </TITLE>
     <style>
-       
+       table, td{
+        border: 1px solid black;
+        border-collapse:collapse;
+        padding:1em;
+       }
     </style>
 </HEAD>
 <BODY>
@@ -49,7 +53,7 @@ while (count($arrayequipos)>0) {
 
     shuffle($arrayequipos);
 
-    //usamos el while para llevar un control sobre las 
+    //usamos el while para llevar un control sobre el numero de elementos que quedan 
     
     $equipo1=array_shift($arrayequipos);//cogemos el primer elemento y lo eliminamos;
     shuffle($arrayequipos);
@@ -63,31 +67,36 @@ while (count($arrayequipos)>0) {
 }
 
 
-
-foreach ($arraypartidos as $key => $value) {
-  echo $value;
-  echo "<br>";
-}
-
 //vamos a crear las apuestas, para ellos vamos a crear un array con las 3 posibilidades
 
 $apuestas=array("1", "2", "x");
 $arrayapuesta = array();
 
 
-    for ($i=1; $i <= (8*14); $i++) { 
+for ($i=1; $i <= (8*14); $i++) { 
     
-        $arrayapuesta[$i-1] = $apuestas[rand(0,2)];
-    }
-
-    $arrayapuesta_partida = array_chunk($arrayapuesta, 8); // lo convertimos en tabla
-
-//print_r($arrayapuesta);
-for ($x=0; $x <=13 ; $x++) {
-    for ($y=0; $y <=7 ; $y++) { 
-        echo $arrayapuesta_partida[$x][$y]." ";
-    }
-    echo "</br>";
+    $arrayapuesta[$i-1] = $apuestas[rand(0,2)];//creamos las apuestas para cada partido en un unico array
 }
 
-print_r($arrayapuesta_partida);
+$arrayapuesta_partida = array_chunk($arrayapuesta, 8); // cogemos el array anterior, y lo convertimos. Vamos a coger cada 8 posiciones, guardarlas todas en un array. 
+//creamos asi un array bidimensional. Por cada fila (1,2,3...) guardamos las apuestas que a su vez estan en un array.
+
+
+//print_r($arrayapuesta);
+
+
+
+//imprimimos con tabla
+echo "<table>";
+
+for ($x=0; $x <=13 ; $x++) {
+    echo "<tr>";
+    echo "<td>" . $arraypartidos[$x] . "</td>";
+    for ($y=0; $y <=7 ; $y++) { 
+
+        echo "<td>" .$arrayapuesta_partida[$x][$y] . "</td>";
+        
+    }
+    echo "</tr>";
+}
+echo "</table>";
