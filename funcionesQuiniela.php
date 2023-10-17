8151207a03b0267c7e7fae1525472e6009336c30
Victor <124076416+Vistaplay16@users.noreply.github.com>
2023-10-17 09:34:11 +0200

    cambios

--- funcionesQuiniela.php
+++ funcionesQuiniela.php
@@ -7,12 +7,51 @@
 </head>
 <body>
     <?php 
+    function main(){
+            $arrayequipos=array("Real Madrid",
+        "FC Barcelona",
+        "Atlético de Madrid",
+        "Valencia CF",
+        "Sevilla FC",
+        "Real Betis",
+        "Villarreal CF",
+        "Real Sociedad",
+        "Athletic Club",
+        "Getafe CF",
+        "Celta de Vigo",
+        "Levante UD",
+        "Espanyol",
+        "Deportivo Alavés",
+        "Real Valladolid",
+        "Granada CF",
+        "SD Eibar",
+        "Mallorca",
+        "Osasuna",
+        "Elche CF",
+        "CD Leganés",
+        "Rayo Vallecano",
+        "Huesca",
+        "Cádiz CF",
+        "RCD Mallorca",
+        "Real Zaragoza",
+        "RC Deportivo",
+        "zaragoza FC"
+    );
+    echo imprimirPartidos($arrayequipos);
+    }
+
     function imprimirPartidos($arrayEquipos){
-        $equipo1=$arrayEquipos[rand(0,count($arrayEquipos))];
         $equipo2=$arrayEquipos[rand(0,count($arrayEquipos))];
-        echo "<td>";
-        echo $equipo1." vs ". $equipo2;
-        echo "</td>";
+        $arrayPartidos=array();
+        for ($i=0; $i < count($arrayEquipos); $i++) { 
+            $equipo1=$arrayEquipos[rand(0,count($arrayEquipos))];
+            $equipo2=$arrayEquipos[rand(0,count($arrayEquipos))];
+            $arrayPartidos=$equipo1. " vs ". $equipo2. "<br>";
+            unset($arrayEquipos[array_search($equipo1, $arrayEquipos)]);
+            unset($arrayEquipos[array_search($equipo1, $arrayEquipos)]);
+        }
+        return $arrayPartidos;
+
     }
     ?>
 </body>
